371ea98d54149bb4913c279bd1fc4e4c6d495009
Ishaan Lodhi <islodhi@syr.edu>
2025-09-04 14:31:06 +0000

    changed landing page

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ lab1 = st.Page("lab_1.py", title="Lab 1", icon="🧪")
 lab2 = st.Page("lab_2.py", title="Lab 2", icon="📄")
 
 # Build the navigation (set Lab 2 as default)
-pg = st.navigation([lab1, lab2], default=lab2)
+pg = st.navigation([lab2, lab1])
 
 # Run the selected page
 pg.run()
