30f016bebdd5e3da43b1992a827cbc09e67a313d
Ishaan Lodhi <islodhi@syr.edu>
2025-09-11 15:06:44 +0000

    updated main page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 # Define your pages
-lab1 = st.Page("lab_1.py", title="Lab 1", icon="🧪")
-lab2 = st.Page("lab_2.py", title="Lab 2", icon="📄")
-lab3 = st.Page("lab_3.py", title = "Lab 3")
+lab1 = st.Page("labs/lab_1.py", title="Lab 1", icon="🧪")
+lab2 = st.Page("labs/lab_2.py", title="Lab 2", icon="📄")
+lab3 = st.Page("labs/lab_3.py", title = "Lab 3")
 
 # Build the navigation (set Lab 2 as default)
 pg = st.navigation([lab2, lab1, lab3])
